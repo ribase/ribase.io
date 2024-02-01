6a7a8e781c8b631e718f30116338eab7bb49cd32
ribase <sthadewald@gmail.com>
2024-02-01 14:44:50 +0100

    changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  appDir: true,
+  experimental: {
+    appDir: true,
+  },
   images: {
     remotePatterns: [
       {
