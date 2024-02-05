661506a60afa4c1ae8ebb0a8d2af4f6b4bfa85e3
ribase <sthadewald@gmail.com>
2024-02-05 14:42:44 +0100

    fixed date

--- src/components/fragments/date.tsx
+++ src/components/fragments/date.tsx
@@ -1,9 +1,11 @@
 import moment from "moment/moment";
+import {useEffect, useState} from "react";
 
 export default function StringToDate({ dateString } : { dateString: string }) {
+    var [date,setDate] = useState(moment(dateString).format('Y'));
     return (
         <>
-            {moment(dateString).format('Y')}
+            {date}
         </>
     )
 }
