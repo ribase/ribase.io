2ca427e4b03d2089de66a6988036307396def6e3
ribase <sthadewald@gmail.com>
2024-02-05 16:28:18 +0100

    fixed image

--- src/components/fragments/date.tsx
+++ src/components/fragments/date.tsx
@@ -3,7 +3,7 @@ import moment from "moment";
 
 export default function StringToDate({ dateString } : { dateString: string }) {
     const [date, setNow] = React.useState<string>(new Date(dateString).toUTCString());
-
+    console.log(dateString);
     return (
         <>
             { moment(date).format('Y') }
--- src/components/front.tsx
+++ src/components/front.tsx
@@ -8,7 +8,7 @@ export default function Front() {
                 <div className="wrapper">
                     <div></div>
                     <div className="circletag" id="nay">
-                        <Image src={"/me.jpg"} alt={"blah"} width={300} height={300}/>
+                        <Image src={"/me.jpg"} alt={"blah"} width={300} />
                     </div>
                     <h1 className="typing-demo">
                         Hello, I&apos;m Sebastian
