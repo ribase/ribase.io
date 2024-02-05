32b70aa608a47ca3d0c50ec94d4a9f86b598a9d0
ribase <sthadewald@gmail.com>
2024-02-05 15:44:08 +0100

    fixed date

--- src/components/fragments/date.tsx
+++ src/components/fragments/date.tsx
@@ -1,8 +1,8 @@
 import moment from "moment/moment";
-import { useEffect } from "react";
+import React from "react";
 
 export default function StringToDate({ dateString } : { dateString: string }) {
-    const date: any = new Date(dateString).getFullYear();
+    const [date, setNow] = React.useState<number>(new Date(dateString).getFullYear());
 
     return (
         <>
