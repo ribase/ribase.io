5cdb2851bbcaced12a4e34f42953180ba921ae1e
ribase <sthadewald@gmail.com>
2024-02-07 11:54:55 +0100

    resource friendlier loading of egg

--- src/components/fragments/easteregg.tsx
+++ src/components/fragments/easteregg.tsx
@@ -1,5 +1,5 @@
 import FormRange from "react-bootstrap/FormRange";
-import React from "react";
+import React, {useEffect} from "react";
 
 export default function Easteregg() {
 
@@ -7,20 +7,19 @@ export default function Easteregg() {
     let tvcontent: any = React.createRef();
     let tvchannel: any = React.createRef();
     let tvvolume: any = React.createRef();
-    let easteregg: any = React.createRef();
     let tvpower: any = React.createRef();
     let tvBg = "black";
     let tvOn = false;
 
 
-    const closeEgg = () => {
-        easteregg.current.style.display="none";
-        tvcontent.current.style.background="black";
-        audio.pause();
-        tvOn = false;
-        tvpower.current.classList.remove("on");
-        tvpower.current.classList.add("off");
-    }
+    useEffect(() => {
+        return () => {
+            if (audio !== null) {
+                audio.pause();
+            }
+
+        }
+    }, [])
 
     const changeVolume = (volume: number) => {
         volume = volume/10;
@@ -93,34 +92,29 @@ export default function Easteregg() {
 
     return (
         <>
-            <div className={"easter--egg"} ref={easteregg}>
-                <div className={"close--egg p-5"} onClick={closeEgg}>
-                    <h2>X</h2>
-                </div>
-                <div className={"easter--egg--container"}>
-                    <div className="old-tv">
-                        <main>
-                            <div className="error-noise">
-                                <div className="error-effect">
-                                    <div className="old-tv-content" ref={tvcontent} style={{background: tvBg}}></div>
-                                </div>
+            <div className={"olf-tv--container"}>
+                <div className="old-tv">
+                    <main>
+                        <div className="error-noise">
+                            <div className="error-effect">
+                                <div className="old-tv-content" ref={tvcontent} style={{background: tvBg}}></div>
                             </div>
-                        </main>
-                        <div className="speaker"></div>
-                        <div className="volume">
-                            <FormRange min={"0"} max={"10"} step={"1"} defaultValue={"1"} ref={tvvolume}
-                                       onChange={(event) => changeVolume(parseInt(event.target.value))}/>
                         </div>
-                        <nav className="channel">
-                            <FormRange min={"0"} max={"2"} step={"1"} defaultValue={"0"} ref={tvchannel}
-                                       onChange={(event) => changeChannel(parseInt(event.target.value))}/>
-                        </nav>
-                        <nav className="power off" ref={tvpower}>
-                            <button onClick={tvButton}></button>
-                        </nav>
-                        <nav></nav>
-                        <footer></footer>
+                    </main>
+                    <div className="speaker"></div>
+                    <div className="volume">
+                        <FormRange min={"0"} max={"10"} step={"1"} defaultValue={"1"} ref={tvvolume}
+                                   onChange={(event) => changeVolume(parseInt(event.target.value))}/>
                     </div>
+                    <nav className="channel">
+                        <FormRange min={"0"} max={"2"} step={"1"} defaultValue={"0"} ref={tvchannel}
+                                   onChange={(event) => changeChannel(parseInt(event.target.value))}/>
+                    </nav>
+                    <nav className="power off" ref={tvpower}>
+                        <button onClick={tvButton}></button>
+                    </nav>
+                    <nav></nav>
+                    <footer></footer>
                 </div>
             </div>
         </>
--- src/components/front.tsx
+++ src/components/front.tsx
@@ -1,23 +1,37 @@
 import Image from "next/image";
 import React, {useState} from "react";
+import FormRange from "react-bootstrap/FormRange";
 import Easteregg from "@/components/fragments/easteregg";
 
 export default function Front() {
-    const [components, setComponents] = useState(["Sample Component"]);
 
-    const revealEgg = () => {
-        setComponents([...components, "Sample Component"])
+    const [show, setShow] = useState(false);
+    const eggContainer: any = React.createRef();
+
+    const toggleEgg = () => {
+        setShow(currentShow => !currentShow);
+        if(eggContainer.current.style.display === "block") {
+            eggContainer.current.style.display = "none"
+        } else {
+            eggContainer.current.style.display = "block"
+        }
     }
 
     // @ts-ignore
     return (
         <>
-            {components.map((item, i) => ( <Easteregg /> ))}
-
+            <div className={"easter--egg"}>
+                <div className={"easter--egg--container"} ref={eggContainer}>
+                    <div className={"close--egg p-5"} onClick={toggleEgg}>
+                        <h2>X</h2>
+                    </div>
+                    { show ? <Easteregg /> : null }
+                </div>
+            </div>
             <div className={"image--overlay"} style={{background: "url(/bmx.jpg) center / cover"}}>
                 <div className="wrapper">
                     <div></div>
-                    <div className="circletag" id="nay" onClick={revealEgg}>
+                    <div className="circletag" id="nay" onClick={toggleEgg}>
                         <Image fill={true} src={"/me.jpg"} alt={"blah"}
                                sizes="{(max-width: 768px) 200p, (max-width: 1200px) 300px}"/>
                     </div>
